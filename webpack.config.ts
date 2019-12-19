a269738c318b0af7b02235bdebd51e1bee7e060a
Robin Radic <rradic@hotmail.com>
2019-12-19 21:17:08 +0100

    update webpack configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,18 @@
-import { PyroBuilder } from '@pyro/webpack';
-import { resolve }     from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import { Builder } from '@anomaly/webpack';
+import { resolve } from 'path';
 
 const path                = (...parts) => resolve(__dirname, ...parts);
-const builder             = new PyroBuilder({
+const builder             = new Builder({
     globs: [
         'vendor/anomaly/streams-platform',
         'addons/*/*/*',
         'core/*/*',
     ],
+    // finder: new AddonFinder(),
+    // mode: process.env.NODE_ENV,
+    // rootPath: process.cwd()
+    // namespace: process.env.WEBPACK_NAMESPACE
 });
 const { wp, env, addons } = builder.init();
 
@@ -22,9 +27,26 @@ const { wp, env, addons } = builder.init();
  * // or
  * let addon = addons.findByComposerName('<composer.json name>')
  */
+wp.extendConfig(config => {
+    config.plugins.push(new CopyWebpackPlugin([
+        {from: path('node_modules/vue/dist/vue.min.js'),to:resolve(builder.options.outputPath, 'js/vue.js')}
+    ], {
 
-wp.blocks.plugins.size(wp)
-
+    }))
+})
+wp.plugin('copy').use(CopyWebpackPlugin, )
+wp.optimization
+    .splitChunks({
+        cacheGroups: {
+            vendors: {
+                name   : 'vendors',
+                test   : /[\\/]node_modules[\\/](inversify|reflect-metadata|core-js|axios|tapable|util|lodash|element-ui|tslib|process|debug|regenerator-runtime|@babel\/runtime)/,
+                enforce: true,
+                chunks : 'initial',
+            },
+        },
+    });
+// wp.resolve.alias.set('@anomaly/streams-platform', path('streams-platform/resources/src'))
 const config = builder.toConfig();
 export { config, builder };
 export default config;
