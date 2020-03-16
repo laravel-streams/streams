555b13db91b51aaf322ab58f08c4e049da223d23
Ryan Thompson <ryan@pyrocms.com>
2020-03-16 16:16:18 -0500

    Eh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
         /**
          * Register additional service
          * providers if they exist.
