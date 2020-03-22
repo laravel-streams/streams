ba8c18ea104f25cdac3bb55e229c8c5666a9c35c
Ryan Thompson <ryan@pyrocms.com>
2020-03-22 18:53:23 -0500

    testing

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,4 +1,19 @@
 {!! img('anomaly.theme.flow::img/nebula.jpg')
-    ->fit(200, 200)
-    ->width('100px')
-    ->alt('Horsehead Nebula - Orion') !!}
+    ->fit(600, 600)
+    ->width('300px')
+    // ->sources([
+    //     '(min-width: 600px)' => [
+    //         'resize' => 400,
+    //         'quality' => 60,
+    //     ],
+    //     '(min-width: 1600px)' => [
+    //         'resize' => 800,
+    //         'quality' => 90,
+    //     ],
+    //     // 'fallback' => [
+    //     //     'resize' => 1800
+    //     // ]
+    // ])
+    ->alt('Horsehead Nebula - Orion')
+    ->img() !!}
+    {{-- ->picture() !!} --}}
