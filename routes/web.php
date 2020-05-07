994c8c7034de2a2dfaaeb5df1e6dcd4fb1821112
Ryan Thompson <service@aiwebsystems.com>
2020-05-07 15:27:38 -0500

    Push it

--- routes/web.php
+++ routes/web.php
@@ -22,28 +22,13 @@ Route::get('/test', function () {
     // it would technically be stored as user_name.json in the directories
     // if user_name.json doesn't exists get will return new empty Document
     $item = $database->get('ryan_thompson');
-    dd($item);
+
     // display property values
-    echo $item->first_name;
-    echo $item->last_name;
+    echo $item->name . '<br>';
     echo $item->email;
 
-    // change existing or add new properties
-    $item->email = 'example@example.com';
-    $item->tags  = ['php', 'developer', 'html5'];
-
     // need to save? thats easy!
-    $item->save();
+    //$item->save();
 
-
-    // check if a record exists and do something if it does or does not
-    if ($database->has('kingslayer')) {
-        // do some action
-    }
-
-    // Need to find all the users that have a tag for "php" ?
-    $users = $database->where('tags', 'IN', 'php')->results();
-
-    // Need to search for all the users who use @yahoo.com email addresses?
-    $users = $database->where('email', 'LIKE', '@yahoo.com')->results();
+    dd($database->where('email', 'LIKE', '@pyrocms.com')->results());
 });
