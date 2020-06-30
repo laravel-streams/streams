d6353c81f796b2cbe40ad33b560cd97c6db54618
Ryan Thompson <ryan@pyrocms.com>
2020-06-30 01:03:11 -0500

    Disabled ones. Keep this for later.

--- config/streams.php
+++ config/streams.php
@@ -28,4 +28,25 @@ return [
         ],
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Addon Customization
+    |--------------------------------------------------------------------------
+    |
+    | Here you can customize and
+    | extend the addon loader.
+    |
+    */
+
+    'addons' => [
+
+        /**
+         * An array of disabled
+         * addons by handle.
+         */
+        'disabled' => [
+            //'anomaly.module.users',
+        ],
+    ],
+
 ];
