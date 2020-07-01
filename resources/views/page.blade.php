09fcfd4d55543ea5a4d643fc096e7517d57c91f8
Ryan Thompson <ryan@pyrocms.com>
2020-07-01 10:36:30 -0500

    K

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,81 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts/default')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <title>Pyro</title>
+    <h1>
+        {{ $entry->title }}
+        @if ($entry->intro)
+            <small>{!! Str::markdown($entry->intro) !!}</small>
+        @endif
+    </h1>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    {!! Str::markdown(View::parse($entry->body, compact('entry'))) !!}
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        svg {
-            height: 50px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            @yield('content')
-        </div>
-    </div>
-</body>
-
-</html>
+@endsection
--- streams/data/pages/testing.md
+++ streams/data/pages/testing.md
@@ -1,3 +1,40 @@
 ---
+enabled: true
+sort: 14
 title: 'Foo Bar Baz'
+doc: getting-started
+docs: 
+    - getting-started
+    - installation
+    - streams
 ---
+
+#### Boolean
+{{ $entry->enabled }}
+
+#### Integer
+{{ $entry->sort }}
+
+#### Text
+{{ $entry->title }}
+
+<hr>
+
+#### Textarea
+{{ $entry->body }}
+
+<hr>
+
+#### Entry
+{{ $entry->expand('doc')->title }}
+
+#### Entries
+@foreach($entry->expand('docs') as $doc)
+- {{ $doc->title }}
+@endforeach
+
+#### Entry::Stream::ToArray
+<pre>{{print_r($entry->stream())}}</pre>
+
+#### Entry::ToArray
+<pre>{{print_r($entry->toArray())}}</pre>
