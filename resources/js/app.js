23b7ca34a32dac3d55b6af44d6b4dc0c185952b7
William Åström <william@pixney.com>
2020-08-14 00:01:53 +0200

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,2 @@
 import "@lottiefiles/lottie-player";
 
-import App from "./components/App.svelte";
-
-const app = new App({
-    target: document.body
-  });
-
-  window.app = app;
-
-export default app;
