59c920e54c9a9767117a54e7c6db39e6c321e4aa
William Åström <william@pixney.com>
2020-09-05 13:45:24 +0200

    .

--- public/js/app.js
+++ public/js/app.js
@@ -3225,22 +3225,18 @@ bashes.forEach(function (bash) {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// https://www.dannyguo.com/blog/how-to-add-copy-to-clipboard-buttons-to-code-blocks-in-hugo/
 if (navigator && navigator.clipboard) {
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
     var button = document.createElement('button');
     var icon = document.createElement('span');
     var clipboard = navigator.clipboard;
     button.className = 'c-copy-code';
-    button.type = 'button'; // button.innerText = 'Copy';
-
+    button.type = 'button';
     button.appendChild(icon);
     codeBlock.parentNode.appendChild(button);
     button.addEventListener('click', function () {
       console.log('clickeds');
       clipboard.writeText(codeBlock.innerText).then(function () {
-        /* Chrome doesn't seem to blur automatically,
-           leaving the button in a focused state. */
         button.blur();
         button.classList.add('c-copy-code__copied');
         setTimeout(function () {
--- resources/js/src/highlight-copy.js
+++ resources/js/src/highlight-copy.js
@@ -1,5 +1,3 @@
-// https://www.dannyguo.com/blog/how-to-add-copy-to-clipboard-buttons-to-code-blocks-in-hugo/
-
 if (navigator && navigator.clipboard) {
     document.querySelectorAll('pre > code').forEach(function (codeBlock) {
         const button = document.createElement('button');
@@ -8,15 +6,13 @@ if (navigator && navigator.clipboard) {
 
         button.className = 'c-copy-code';
         button.type = 'button';
-       // button.innerText = 'Copy';
         button.appendChild(icon);
         codeBlock.parentNode.appendChild(button);
 
         button.addEventListener('click', () => {
             console.log('clickeds')
             clipboard.writeText(codeBlock.innerText).then(function () {
-                /* Chrome doesn't seem to blur automatically,
-                   leaving the button in a focused state. */
+               
                 button.blur();
 
                 button.classList.add('c-copy-code__copied')
