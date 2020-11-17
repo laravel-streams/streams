da29769a740337fad9228b1db0187561f20f5a00
Ryan Thompson <ryan@pyrocms.com>
2020-11-17 01:00:35 -0600

    Nope

--- routes/cp.php
+++ routes/cp.php
@@ -11,70 +11,8 @@
 |
 */
 
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\Finder\SplFileInfo;
-use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
-use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
-use Anomaly\Streams\Platform\Streams\Facades\Streams;
 
-Route::any('/', function () {
-
-    $table = (new TableBuilder([
-        'columns' => [
-            'name' => [
-                'heading' => 'Name',
-                'sortable' => true,
-                'value' => 'entry.name'
-            ],
-        ],
-        'buttons' => [
-            'entries' => [
-                'href' => 'admin/{entry.slug}',
-            ],
-        ],
-    ]));
-
-    $table->on('build', function($workflow) use ($table) {
-
-        $workflow->on('before_query_entries', function() use ($table) {
-
-            array_map(function(SplFileInfo $item) use ($table) {
-                $table->instance->entries->push([
-                    'name' => Streams::try($item->getBasename('.json'), function($stream) {
-                        return $stream->name;
-                    }) ?? ucwords(Str::humanize($item->getBasename('.json'), '-')),
-                    'slug' => $item->getBasename('.json'),
-                ]);
-            }, File::files(base_path('streams')));
-        });
-    });
-
-    return $table->response();
-});
-
-Route::any('/{stream}', function ($stream) {
-
-    $table = (new TableBuilder([
-        'stream' => $stream,
-        'columns' => Streams::make($stream)->fields->keys()->all(),
-        'buttons' => [
-            'edit' => [
-                'href' => 'admin/{request.segments.1}/{entry.id}',
-            ],
-        ],
-    ]));
-
-    return $table->response();
-});
-
-Route::any('/{stream}/{id}', function ($stream, $id) {
-
-    $form = (new FormBuilder([
-        'stream' => $stream,
-        'entry' => $id,
-    ]));
-
-    return $form->response();
-});
+// Route::streams('/{whatever}', [
+//     //
+// ]);
