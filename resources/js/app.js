d7343be76183ac672e5b907267fcea147ed4ab7e
Ryan Thompson <service@aiwebsystems.com>
2020-12-04 11:50:00 -0600

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 
-import "./src/highlight"
-import "./src/highlight-copy"
+// import "./src/highlight"
+// import "./src/highlight-copy"
 
-const bashes = document.querySelectorAll('.language-bash');
-bashes.forEach(bash => {
-    bash.closest('pre').classList.add('language-bash');
-    bash.classList.add('c-scrollbar');
-});
+// const bashes = document.querySelectorAll('.language-bash');
+// bashes.forEach(bash => {
+//     bash.closest('pre').classList.add('language-bash');
+//     bash.classList.add('c-scrollbar');
+// });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,6 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-
-// Do we want notifications?
-mix.disableSuccessNotifications();
-
-// Transpile js and sass
 mix.js('resources/js/app.js', 'js')
     .sass('resources/sass/theme.scss', 'css');
 
@@ -15,22 +10,14 @@ mix.options({
 });
 
 
-mix.browserSync({
-    proxy: process.env.APP_URL,
-    files: [
-        'public/js/**/*.js',
-        'public/css/**/*.css',
-        'resources/views/**/*.html',
-        'streams/**/*.json',
-        'streams/**/*.md'
-    ],
-    notify: false
-});
-
-
-mix.version();
-
-// Purge css 
-if (mix.inProduction()) {
-    mix.sourceMaps().version();
-}
+// mix.browserSync({
+//     proxy: process.env.APP_URL,
+//     files: [
+//         'public/js/**/*.js',
+//         'public/css/**/*.css',
+//         'resources/views/**/*.html',
+//         'streams/**/*.json',
+//         'streams/**/*.md'
+//     ],
+//     notify: false
+// });
