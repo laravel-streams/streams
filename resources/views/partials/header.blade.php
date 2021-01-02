72b81c014635432937b8acca5085b8acdff96851
Ryan Thompson <ryan@pyrocms.com>
2021-01-01 21:46:12 -0600

    Progress

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,25 +3,15 @@
     <div class="flex items-center justify-between px-20 py-6">
         
         <div class="flex items-center justify-center">
-            <div class="flex items-center justify-center text-3xl font-bold text-gray-500">
+            
+            <div class="flex items-center justify-center font-bold text-black text-3xl">
                 Laravel Streams
             </div>   
 
-            <div class="hidden lg:flex items-center justify-center antialiased lg:ml-20 pt-1">
-                <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
-                Product
-                <svg class="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
-                </svg>
-                </a>
+            <div class="hidden relative lg:flex items-center justify-center antialiased lg:ml-20 pt-1">
                 <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
                 Community
-                <svg class="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
-                </svg>
-                </a>
-                <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
-                Plans
+                <x-fas-chevron-down class="h-3 ml-2"/>
                 </a>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
     
-    <ul>
-        <li>Introduction</li>
-        <li>Mission</li>
-        <li>Documentation</li>
-    </ul>  
+    <div class="py-6 px-20 relative">
+
+        <h1 class="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">A new breed of CMS.</h1>
+
+        <p class="max-w-screen-lg text-lg text-gray-700 sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">A code-configured data management system loaded with features like <code class="font-mono text-black font-bold ">data modeling</code>, <code class="font-mono text-black font-bold ">automagic UI</code>, and a built-in <code class="font-mono text-black font-bold ">REST API</code> that can be used to build anything.</p>
+        
+        <ul class="list-disc pl-4">
+            <li>Introduction</li>
+            <li>Documentation</li>
+            <li>Contribute</li>
+            <ul class="list-disc pl-4">
+                <li note="https://www.iconfinder.com/#contributors">Become a contributor</li>
+                <li note="https://github.com/sponsors/RyanThompson">Sponsor the project</li>
+            </ul>
+        </ul>
+    </div>
 
 @endsection
