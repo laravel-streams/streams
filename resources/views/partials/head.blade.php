c0f98994656c11507f4314fa0ac8124b175b915e
Ryan Thompson <ryan@pyrocms.com>
2021-02-05 11:07:56 -0600

    accessibility

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,7 +6,7 @@
 <meta http-equiv="Content-Language" content="{{ config('app.locale') }}">
 
 <meta name="generator" content="Laravel Streams"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <meta name="description" content="{{ config('app.description') }}"/>
