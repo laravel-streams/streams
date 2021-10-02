4003b6ca1a9387ccab6eb4d0a92c56ee8d1aa4fa
Robin Radic <RobinRadic@users.noreply.github.com>
2021-10-02 09:34:17 +0200

    Update assets.blade.php

--- resources/views/partials/assets.blade.php
+++ resources/views/partials/assets.blade.php
@@ -1,18 +1,32 @@
 {!! Assets::tag('/vendor/streams/core/js/index.js') !!}
 {!! Assets::tag('/vendor/streams/api/js/index.js') !!}
+{!! Assets::tag('/vendor/streams/ui/js/index.js') !!}
 
 {!! Assets::tag('/js/app.js') !!}
 
 <script>
-    window.streams.core.app.bootstrap({
-        providers: [
-            window.streams.core.StreamsServiceProvider,
-            //window.streams.api.ApiServiceProvider,
-            //window.app.AppServiceProvider
-        ]
-    }).then(app => {
-        return app.boot();
-    }).then(app => {
-        return app.start();
-    });
+(function () {
+    var app = window.streams.core.app;
+    app
+        .initialize({
+            providers: [
+                window.streams.core.HttpServiceProvider,
+                window.streams.api.StreamsServiceProvider,
+                window.streams.ui.UiServiceProvider,
+                window.app.AppServiceProvider
+            ],
+            config   : {
+                streams: {},
+                http   : {}
+            }
+        })
+        .then(app.boot.bind(app))
+        .then(function (app) {
+            app.start();
+        })
+        .then(function (app) {
+            // started
+        });
+}).call(window);
 </script>
+
