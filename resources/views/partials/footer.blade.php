10b8612a1a8855d8742de2c79d914d84d5312e7b
Ryan Thompson <ryan@pyrocms.com>
2021-12-04 01:52:52 -0600

    Links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,34 +1,16 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <footer class="bg-white">
   <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+
     <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
 
       <div class="px-5 py-2">
-        <a href="https://streams.dev" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="https://streams.dev" target="_blank" class="text-base text-gray-500 hover:text-gray-900">
           About
         </a>
       </div>
 
-      {{-- <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-          Blog
-        </a>
-      </div> --}}
-
-      {{-- <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-          Jobs
-        </a>
-      </div> --}}
-
-      {{-- <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-          Press
-        </a>
-      </div> --}}
-
       <div class="px-5 py-2">
-        <a href="https://streams.dev/docs" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="https://streams.dev/docs" target="_blank" class="text-base text-gray-500 hover:text-gray-900">
           Documentation
         </a>
       </div>
@@ -37,16 +19,7 @@
 
     <div class="mt-8 flex justify-center space-x-6">
 
-      <a href="#" class="text-gray-400 hover:text-gray-500">
-        <span class="sr-only">Facebook</span>
-        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-          <path fill-rule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clip-rule="evenodd" />
-        </svg>
-      </a>
-
-      <a href="#" class="text-gray-400 hover:text-gray-500">
+      <a href="https://www.instagram.com/ryanthepyro/" target="_blank" class="text-gray-400 hover:text-gray-500">
         <span class="sr-only">Instagram</span>
         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
           <path fill-rule="evenodd"
@@ -55,7 +28,7 @@
         </svg>
       </a>
 
-      <a href="#" class="text-gray-400 hover:text-gray-500">
+      <a href="https://twitter.com/Laravel_Streams" target="_blank" class="text-gray-400 hover:text-gray-500">
         <span class="sr-only">Twitter</span>
         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
           <path
@@ -63,7 +36,7 @@
         </svg>
       </a>
 
-      <a href="#" class="text-gray-400 hover:text-gray-500">
+      <a href="https://github.com/laravel-streams/streams" target="_blank" class="text-gray-400 hover:text-gray-500">
         <span class="sr-only">GitHub</span>
         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
           <path fill-rule="evenodd"
@@ -72,19 +45,10 @@
         </svg>
       </a>
 
-      <a href="#" class="text-gray-400 hover:text-gray-500">
-        <span class="sr-only">Dribbble</span>
-        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-          <path fill-rule="evenodd"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-            clip-rule="evenodd" />
-        </svg>
-      </a>
-
     </div>
-    
+
     <p class="mt-8 text-center text-base text-gray-400">
-      &copy; {{ date('Y') }} {{ config('app.name') }}, All rights reserved.
+      &copy; {{ date('Y') }} {{ config('app.name') }}, No rights reserved.
     </p>
 
   </div>
