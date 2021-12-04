c5b354627f43c0eecb27db3ca25833205e6a4e1b
Ryan Thompson <ryan@pyrocms.com>
2021-12-04 01:48:39 -0600

    Package.json is broken as all get out.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: false,
         content: [
             './storage/framework/views/*.php',
             './resources/**/*.blade.php',
