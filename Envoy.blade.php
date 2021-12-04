a3638e7ffc8f7ff209b02ae7bba2cdfc4ddfc8df
Ryan Thompson <ryan@pyrocms.com>
2021-12-04 02:50:04 -0600

    Working Envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -10,22 +10,22 @@ $dotenv = \Dotenv\Dotenv::create(
 );
 
 try {
-$dotenv->load();
-$dotenv->required(['DEPLOY_SERVER', 'DEPLOY_REPOSITORY', 'DEPLOY_PATH'])->notEmpty();
+    $dotenv->load();
+    $dotenv->required(['DEPLOY_SERVER', 'DEPLOY_REPOSITORY', 'DEPLOY_PATH'])->notEmpty();
 } catch ( Exception $e )  {
-echo $e->getMessage();
+    echo $e->getMessage();
 }
 
-$server = getenv('DEPLOY_SERVER');
-$repo = getenv('DEPLOY_REPOSITORY');
-$path = getenv('DEPLOY_PATH');
-$slack = getenv('DEPLOY_SLACK_WEBHOOK');
-$healthUrl = getenv('DEPLOY_HEALTH_CHECK');
+$server = env('DEPLOY_SERVER');
+$repo = env('DEPLOY_REPOSITORY');
+$path = env('DEPLOY_PATH');
+$slack = env('DEPLOY_SLACK_WEBHOOK');
+$healthUrl = env('DEPLOY_HEALTH_CHECK');
 
-$php = getenv('DEPLOY_PHP_BINARY') ?: 'php';
-$mysql = getenv('DEPLOY_MYSQL_BINARY') ?: 'mysql';
-$composer = getenv('DEPLOY_COMPOSER_BINARY') ?: 'composer';
-$defaultBranch = getenv('DEPLOY_DEFAULT_BRANCH') ?: 'master';
+$php = env('DEPLOY_PHP_BINARY') ?: 'php';
+$mysql = env('DEPLOY_MYSQL_BINARY') ?: 'mysql';
+$composer = env('DEPLOY_COMPOSER_BINARY') ?: 'composer';
+$defaultBranch = env('DEPLOY_DEFAULT_BRANCH') ?: 'master';
 
 if ( substr($path, 0, 1) !== '/' ) throw new Exception('Your deployment must begin with /');
 
@@ -34,214 +34,101 @@ $env = isset($env) ? $env : "production";
 $branch = isset($branch) ? $branch : $defaultBranch;
 $update = isset($update) ? $update : false;
 $path = rtrim($path, '/');
-$release = $path.'/'.$date;
 @endsetup
 
 @servers(['web' => $server])
 
 @task('init')
-if [ ! -d {{ $path }}/current ]; then
+if [ ! -d {{ $path }} ]; then
 cd {{ $path }}
-git clone {{ $repo }} --branch={{ $branch }} --depth=1 -q {{ $release }}
+git clone {{ $repo }} --branch={{ $branch }} --depth=1 -q {{ $path }}
 echo "Repository cloned"
-if [ ! -d {{ $path }}/storage ]; then
-mv {{ $release }}/storage {{ $path }}/storage
-else
-rm -Rf {{ $release }}/storage
-fi
-ln -s {{ $path }}/storage {{ $release }}/storage
-echo "Storage directory set up"
-if [ ! -d {{ $path }}/.env ]; then
 cp {{ $path }}/.env.example {{ $path }}/.env
 {{ $php }} {{ $path }}/artisan key:generate --ansi
 fi
-ln -s {{ $path }}/.env {{ $release }}/.env
+ln -s {{ $path }}/.env {{ $path }}/.env
 echo "Environment file set up"
-rm -rf {{ $release }}
-echo "Deployment path initialised. Run 'envoy run deploy' now."
-else
-echo "Deployment path already initialised (current symlink exists)!"
-fi
+echo "Deployment initialised. Run 'envoy run deploy' now."
 @endtask
 
 @story('push')
-current_pull
-current_refresh
+pull
+refresh
 health_check_ping
 @endstory
 
 @story('revert')
-current_revert
-current_refresh
+revert
+refresh
 health_check_ping
 @endstory
 
 @story('refresh')
-current_refresh
+refresh
 @endstory
 
-@story('build')
-current_build
-@endstory
+@task('composer')
+echo "Installing composer dependencies."
+cd {{ $path }}
+{{ $composer }} install --no-interaction --quiet --no-dev --prefer-dist --optimize-autoloader --ignore-platform-reqs
+@endtask
+
+@task('migrate')
+{{ $php }} {{ $path }}/artisan migrate --env={{ $env }} --force --no-interaction
+@endtask
 
 @story('deploy')
-@if ( isset($backup) && $backup )
+{{-- @if ( isset($backup) && $backup )
     deployment_backup
-@endif
-deployment_start
-deployment_links
-deployment_composer
-deployment_migrate
-deployment_build
-deployment_finish
-deployment_refresh
-deployment_complete
+@endif --}}
+pull
+composer
+migrate
+refresh
 health_check_ping
-@if ( isset($cleanup) && $cleanup )
-    deployment_cleanup
-@endif
-@endstory
-
-@story('cleanup')
-deployment_cleanup
 @endstory
 
 @story('rollback')
-deployment_rollback
+revert
+composer
+rollback
 health_check_ping
 @endstory
 
-@story('health_check')
+{{-- @story('health_check')
 health_check_ping
-@endstory
+@endstory --}}
 
-@task('current_pull')
-cd {{ $path }}/current
+@task('pull')
+cd {{ $path }}
 git pull
 @endtask
 
-@task('current_revert')
-cd {{ $path }}/current
+@task('revert')
+cd {{ $path }}
 echo "Revert started"
 git reset --keep HEAD@{1}
 @endtask
 
-@task('deployment_start')
-cd {{ $path }}
-echo "Deployment ({{ $date }}) started"
-
-git clone {{ $repo }} --branch={{ $branch }} --depth=1 -q {{ $release }}
-echo "Repository [{{ $branch }}] cloned"
+@task('refresh')
+#{{ $php }} {{ $path }}/artisan refresh --quiet
+#echo "System refreshed"
 @endtask
 
-@task('deployment_backup')
-cd {{ $path }}/current
-echo "Database backup started"
+@task('rollback')
+cd {{ $path }}
+{{ $php }} {{ $path }}/artisan migrate:rollback --env={{ $env }} --force --no-interaction
+echo "Rolled back."
+@endtask
 
-DB_USERNAME=$(grep DB_USERNAME .env | xargs)
-IFS='=' read -ra DB_USERNAME <<< "$DB_USERNAME"
-DB_USERNAME=${DB_USERNAME[1]}
-
-DB_PASSWORD=$(grep DB_PASSWORD .env | xargs)
-IFS='=' read -ra DB_PASSWORD <<< "$DB_PASSWORD"
-DB_PASSWORD=${DB_PASSWORD[1]}
-
-DB_DATABASE=$(grep DB_DATABASE .env | xargs)
-IFS='=' read -ra DB_DATABASE <<< "$DB_DATABASE"
-DB_DATABASE=${DB_DATABASE[1]}
-
-DB_HOST=$(grep DB_HOST .env | xargs)
-IFS='=' read -ra DB_HOST <<< "$DB_HOST"
-DB_HOST=${DB_HOST[1]}
-
-/bin/cat <<EOM >database.cnf
-    [client]
-    password="${DB_PASSWORD[1]}"
-    EOM
-
-    mysqldump --defaults-extra-file={{ $path }}/current/database.cnf -u ${DB_USERNAME} ${DB_DATABASE} > database.sql
-
-    rm database.cnf
-
-    echo "Database backup complete"
-    @endtask
-
-    @task('deployment_links')
-    cd {{ $path }}
-    rm -rf {{ $release }}/storage
-    ln -s {{ $path }}/storage {{ $release }}/storage
-    echo "Storage directories set up"
-    ln -s {{ $path }}/.env {{ $release }}/.env
-    echo "Environment file set up"
-    @endtask
-
-    @task('deployment_composer')
-    echo "Installing composer dependencies."
-    cd {{ $release }}
-    {{ $composer }} install --no-interaction --quiet --no-dev --prefer-dist --optimize-autoloader
-    @endtask
-
-    @task('deployment_migrate')
-    {{ $php }} {{ $release }}/artisan migrate --env={{ $env }} --force --no-interaction --path=vendor/anomaly/streams-platform/migrations/application
-    {{ $php }} {{ $release }}/artisan migrate --env={{ $env }} --all-addons --force --no-interaction
-    {{ $php }} {{ $release }}/artisan migrate --env={{ $env }} --force --no-interaction
-    @endtask
-
-    @task('deployment_build')
-    {{ $php }} {{ $release }}/artisan build --quiet
-    echo "System built"
-    @endtask
-
-    @task('deployment_rebuild')
-    {{ $php }} {{ $release }}/artisan build --quiet
-    echo "System built"
-    @endtask
-
-    @task('deployment_refresh')
-    #{{ $php }} {{ $release }}/artisan refresh --quiet
-    #echo "System refreshed"
-    @endtask
-
-    @task('current_refresh')
-    #{{ $php }} {{ $path }}/current/artisan refresh --quiet
-    #echo "System refreshed"
-    @endtask
-    
-    @task('current_build')
-    echo "System building."
-    {{ $php }} {{ $path }}/current/artisan build
-    echo "System built."
-    @endtask
-
-    @task('deployment_finish')
-    ln -s {{ $release }} {{ $path }}/current
-    @endtask
-
-    @task('deployment_complete')
-    echo "Deployment ({{ $date }}) finished"
-    @endtask
-
-    @task('deployment_cleanup')
-    cd {{ $path }}
-    find . -maxdepth 1 -name "20*" | sort | head -n -4 | xargs rm -Rf
-    echo "Cleaned up old deployments"
-    @endtask
-
-    @task('deployment_rollback')
-    cd {{ $path }}
-    ln -nfs {{ $path }}/$(find . -maxdepth 1 -name "20*" | sort  | tail -n 2 | head -n1) {{ $path }}/current
-    {{ $php }} {{ $release }}/artisan migrate:rollback --env={{ $env }} --force --no-interaction
-    echo "Rolled back to $(find . -maxdepth 1 -name "20*" | sort  | tail -n 2 | head -n1)"
-    @endtask
-
-    @task('health_check_ping')
-    @if ( ! empty($healthUrl) )
-        if [ "$(curl --write-out "%{http_code}\n" --silent --output /dev/null {{ $healthUrl }})" == "200" ]; then
-        printf "\033[0;32mHealth check to {{ $healthUrl }} OK\033[0m\n"
-        else
-        printf "\033[1;31mHealth check to {{ $healthUrl }} FAILED\033[0m\n"
-        fi
-    @else
+@task('health_check_ping')
+@if ( ! empty($healthUrl) )
+    if [ "$(curl --write-out "%{http_code}\n" --silent --output /dev/null {{ $healthUrl }})" == "200" ]; then
+    printf "\033[0;32mHealth check to {{ $healthUrl }} OK\033[0m\n"
+    else
+    printf "\033[1;31mHealth check to {{ $healthUrl }} FAILED\033[0m\n"
+    fi
+@else
         echo "Ping Skipped: [DEPLOY_HEALTH_CHECK] URL not set"
 @endif
 @endtask
